fede29edf323918c3a3c6c4efc2e872b9059cff9
Aamir Zargar <aamirzargar@hotmail.com>
2025-08-12 22:48:17 +0530

    Day 21 Installed Ui packages to create the users for the application

--- SalonManager/Program.cs
+++ SalonManager/Program.cs
@@ -1,33 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-
-builder.Services.AddDbContext<AppDbContext>
-    (options =>
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>
-    (options => {
+// Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
     options.Password.RequiredLength = 6;
 })
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -36,7 +31,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();         
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
