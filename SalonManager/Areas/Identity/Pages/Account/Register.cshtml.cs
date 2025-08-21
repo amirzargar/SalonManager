bfcaa3c050d895314531128ab33054a6eb5642ed
Aamir Zargar <aamirzargar@hotmail.com>
2025-08-22 00:32:35 +0530

    feat(identity): auto-assign "Customer" role on user registration
    - Modified Register.cshtml.cs to add new users to "Customer" role automatically
    - Ensured seamless access to customer features upon registration
    - Verified correct role assignment through testing
    - Improved onboarding flow by removing manual admin role assignment

--- SalonManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SalonManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -124,6 +124,9 @@ namespace SalonManager.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    // Automatically assign "Customer" role here:
+                    await _userManager.AddToRoleAsync(user, "Customer");
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
